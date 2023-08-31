4064e846642b08989ff99499432c3b5f746500e6
smile0902 <rladjstn@dgsw.hs.kr>
2023-08-31 09:03:02 +0900

    ✅ :: 프로젝트 세팅

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WebButtonPage from "./views/WebButton/WebButtonPage";
-import WebTextFieldPage from "./views/WebTextField/WebTextFieldPage";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/web-button" element={<WebButtonPage />}></Route>
-        <Route exact path="/web-textfield" element={<WebTextFieldPage />}></Route>
+        <Route exact path="/" element={<></>}></Route>
       </Routes>
     </BrowserRouter>
   );
